eb542aaa03c99bfc421e7af8ba9abe0ad15dc06d
Scott Abbott <wolfe@Scotts-MacBook-Pro.local>
2019-01-17 09:13:23 -0700

    Restored some files, fixed some errors.

--- AStar/Constants.cs
+++ AStar/Constants.cs
@@ -6,9 +6,9 @@ namespace AStar
     {
         public Constants(){}
 
-        public static const int NUM_ROWS = 5;
-        public static const int NUM_CLMS = 8;
-        public static const int HOR_VERT_COST = 10;
-        public static const int DIAG_COST = 14; 
+        public static readonly int NUM_ROWS = 5;
+        public static readonly int NUM_CLMS = 8;
+        public static readonly int HOR_VERT_COST = 10;
+        public static readonly int DIAG_COST = 14; 
     }
 }
